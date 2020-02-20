0c17d88144d5b36a38bbc050a319f9ef8fa75255
Kajigga Dev <kajigga@gmail.com>
2020-02-20 14:11:58 -0700

    update makefile and .gitignore

--- Makefile
+++ Makefile
@@ -1,7 +1,10 @@
-APP_NAME="go-example-app"
+APP_NAME=go-example-app
 
 go-example-app:
 	go build -o dist/${APP_NAME} .
+
+releaser:
+	/usr/local/bin/goreleaser release --skip-publish
 
 clean:
 	go clean
--- exampleapp.go
+++ exampleapp.go
@@ -1,9 +1,33 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"fmt"
+)
+
+const VERSION = "0.4"
+
+type Args struct {
+	GetVersion bool
+	Message    string
+}
 
 func main() {
 
-	fmt.Println("Hello World")
+	a := Args{}
+	flag.BoolVar(&a.GetVersion, "version", false, "get the version")
+	flag.StringVar(&a.Message, "message", "", "message to share")
 
+	flag.Parse()
+
+	if a.GetVersion {
+		version()
+	} else {
+		fmt.Println(a.Message)
+	}
+
+}
+
+func version() {
+	fmt.Println(VERSION)
 }
